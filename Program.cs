d77a23ba4a04744527f5ffca1677b4cf54dfd0f7
Apk <146819953+7NRVHXY@users.noreply.github.com>
2023-10-31 20:25:50 +0400

    created2

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ifelseucbucag
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int side1 = 5;
+            int side2 = 8;
+            int side3 = 13;
+            if (side1 + side2 >side3 && side1+side3 >side2 && side2+side3 >side1) 
+            {
+                Console.WriteLine("duzgun ucbucagdir");
+            }
+            else
+            {
+                Console.WriteLine("duzgun ucbucag deyil");
+            }
+        }
+    }
+}
